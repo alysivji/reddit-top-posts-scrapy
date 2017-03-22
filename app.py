21a423143b369105cab60b40cac71dac16eaf3d3
Aly Sivji <alysivji@gmail.com>
2017-03-22 14:33:57 -0500

    Switch Mongo instance to mlab and use configparser to get URI

--- app.py
+++ app.py
@@ -3,12 +3,11 @@
 
 from scrapy.crawler import CrawlerProcess
 from reddit.spiders import PostSpider
+from scrapy.utils.project import get_project_settings
 from top_post_emailer import email_last_scraped_date
 
 if __name__ == '__main__':
-    process = CrawlerProcess({
-        'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
-    })
+    process = CrawlerProcess(get_project_settings())
 
     process.crawl(PostSpider)
     process.start() # the script will block here until the crawling is finished
--- top_post_emailer/__init__.py
+++ top_post_emailer/__init__.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python3
 
+import os
+import configparser
 from mongoengine.connection import connect
 from .data_model import Post
 from .render_template import render
 from .mailgun_emailer import send_email
 
 def email_last_scraped_date():
+    ## mongodb params (using configparser)
+    config = configparser.ConfigParser()
+    config.read(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'settings.cfg'))
+    mlab_uri = config.get('MongoDB', 'mlab_uri')
+
     # connect to db
-    MONGO_URI = 'mongodb://localhost:27017'
-    connect('sivji-sandbox', host=MONGO_URI)
+    MONGO_URI = mlab_uri
+    connect('sivji-sandbox', host=MONGO_URI)    
 
     ## get the last date the webscraper was run
     for post in Post.objects().fields(date_str=1).order_by('-date_str').limit(1):
