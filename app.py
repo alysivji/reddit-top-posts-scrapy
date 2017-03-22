90f1e93cb2bfd4195bf13233765804668b0c63ad
Aly Sivji <alysivji@gmail.com>
2017-03-22 18:29:42 -0500

    Set to run once a week

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 """Script to crawl Top Posts across sub reddits and store results in MongoDB
 """
 
+from datetime import date
 from scrapy.crawler import CrawlerProcess
 from reddit.spiders import PostSpider
 from scrapy.utils.project import get_project_settings
 from top_post_emailer import email_last_scraped_date
 
 if __name__ == '__main__':
-    process = CrawlerProcess(get_project_settings())
+    # only run on saturdays (once a week)
+    if date.strftime(date.today(), '%A').lower() == 'saturday':
+        process = CrawlerProcess(get_project_settings())
 
-    process.crawl(PostSpider)
-    process.start() # the script will block here until the crawling is finished
+        process.crawl(PostSpider)
+        process.start() # the script will block here until the crawling is finished
 
-    email_last_scraped_date()
+        email_last_scraped_date()
