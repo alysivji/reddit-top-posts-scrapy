99d7191d05c9edd9215bfaff5e042eadbf075877
Aly Sivji <alysivji@gmail.com>
2017-03-31 20:32:39 -0500

    Added logging

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 """Script to crawl Top Posts across sub reddits and store results in MongoDB
 """
 
+import logging
 from datetime import date
 from scrapy.crawler import CrawlerProcess
 from reddit.spiders import PostSpider
@@ -8,11 +9,17 @@ from scrapy.utils.project import get_project_settings
 from top_post_emailer import email_last_scraped_date
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+
     # only run on saturdays (once a week)
     if date.strftime(date.today(), '%A').lower() == 'saturday':
-        process = CrawlerProcess(get_project_settings())
+        crawler = CrawlerProcess(get_project_settings())
 
-        process.crawl(PostSpider)
-        process.start() # the script will block here until the crawling is finished
+        crawler.crawl(PostSpider)
+        crawler.start() # the script will block here until the crawling is finished
 
         email_last_scraped_date()
+        logger.info('Scrape complete and email sent.')
+    else:
+        logger.info('Script did not run.')
